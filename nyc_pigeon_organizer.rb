a757046dedf499bce47aabeeedfed4e1cb1ae58b
Ethan Davidson <theejdavidson@gmail.com>
2020-02-02 00:17:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+triple_list = data.reduce([]) do |triple_list, (characteristic, value_hash) |
+  value_hash.reduce(triple_list) do |triple_list, (value, birds)|
+    birds.reduce(triple_list) do |memo, bird|
+      memo << [characteristic, value.to_s, bird]
+    end
+  end
+end
+
 
 end
